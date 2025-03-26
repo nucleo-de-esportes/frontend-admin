8da79c0af3dcad71f33148aba76cc410271b42de
Alexsandro <jalexsandro2005@gmail.com>
2025-03-25 21:43:55 -0300

    criado componente de botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 //import { useState } from 'react'
 
+import Button from "./components/Button"
+import { FaSave, FaTrash } from "react-icons/fa";
+
 function App() {
   return (
     <>
-      <div className='h-screen w-screen flex items-center justify-center'>
-        <h1 className=" font-bold text-4xl text-blue-700">Hello, World!</h1>
+      <div className='h-screen w-screen flex flex-col items-center gap-4 justify-center'>
+
+        <Button minWidth="21rem" text="Confirmar" onClick={() => console.log("Confirmado!")} />
+
+        <div className="flex gap-4">
+          <Button minWidth="10rem" text="Salvar" icon={FaSave} color="#36B37E" onClick={() => console.log("Salvo!")} />
+          <Button minWidth="10rem" text="Remover" icon={FaTrash} color="#D81E5B" onClick={() => console.log("Removido!")} />
+        </div>
+        
       </div>
     </>
   )
