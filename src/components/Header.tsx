51952c954372c32ca17f8ef0f871d8cef449d3ef
Alexsandro <jalexsandro2005@gmail.com>
2025-04-01 21:58:48 -0300

    alterado logo do ceub

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import ceubLogo from "/Logo_Ceub_Negativa_Lilas.png"; 
+import ceubLogo from "/logo-ceub.png"; 
 
 const Header = () => {
   return (
-    <header className="bg-[#43054E] h-24 justify-center md:justify-start flex items-center md:px-40 w-full">
-      <img src={ceubLogo} alt="CEUB Logo" className="h-26 w-auto" />
+    <header className="bg-[#43054E] h-24 justify-center md:justify-start gap-6 flex items-center md:px-40 w-full">
+      <img src={ceubLogo} alt="CEUB Logo" className="h-12 w-auto" />
       <h1 className="text-white md:text-3xl font-semibold">
         Núcleo de Esportes
       </h1>
